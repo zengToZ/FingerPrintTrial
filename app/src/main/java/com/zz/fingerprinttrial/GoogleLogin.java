108f47623ebf4e81d5d42d7df5f1a8833d71ab12
zengToZ <zengzhuang@gmail.com>
2018-12-14 23:52:02 +0800

    2nd

---
+++ app/src/main/java/com/zz/fingerprinttrial/GoogleLogin.java
@@ -0,0 +1,13 @@
+package com.zz.fingerprinttrial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoogleLogin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_login);
+    }
+}
